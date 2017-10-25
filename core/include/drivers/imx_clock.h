8d981764f5ebcdac18ed73d63dfa9ab9550d0c59
Dan Mihai <Daniel.Mihai@microsoft.com>
2017-10-25 11:52:05 -0700

    Add ability to query ECSPI clock
    Add ability to query ECSPI clock frequency. The new get_cspi_clk()
    will be used by the upcoming ECSPI driver.
    
    Signed-off-by: Dan Mihai <Daniel.Mihai@microsoft.com>

---
+++ core/include/drivers/imx_clock.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2010-2011 Freescale Semiconductor, Inc.
+ *
+ * Copyright (C) 2017 Microsoft - adapted from:
+ *
+ * u-boot\arch\arm\cpu\armv7\mx6\clock.c
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ASM_ARCH_CLOCK_H
+#define __ASM_ARCH_CLOCK_H
+
+unsigned int get_cspi_clk(void);
+
+#endif /* __ASM_ARCH_CLOCK_H */

fbd72af4e0e87ca97530e622a2263f2d1571f5d8
Dan Mihai <Daniel.Mihai@microsoft.com>
2017-10-26 16:00:03 -0700

    Change the default console back to UART1
    Change the default console to UART1, to be back compatible with the
    behavior before commit 241d510311f2ee8e873cc578b3c841837250eaff
    
    Signed-off-by: Dan Mihai <Daniel.Mihai@microsoft.com>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -65,7 +65,7 @@ ifeq ($(PLATFORM_FLAVOR), mx6qhmbedge)
 # In the default configuration: 
 # - UART3 is used for OPTEE console
 # - UART1 is used for Windows kernel debugging
-CFG_CONSOLE_UART ?= UART3_BASE
+CFG_CONSOLE_UART ?= UART1_BASE
 
 # IOMUX initialization is not supported by default.
 CFG_IMX_IOMUX ?= n

00998b2b5bd8599285386b35c5e69e762d95b5ca
Daniel Mihai (WINDOWS) <dmihai@ntdev.microsoft.com>
2017-11-22 14:07:15 -0800

    Reduce DMSG output
    Move excessive output from DEBUG to FLOW level.

--- core/tee/tee_fs_key_manager.c
+++ core/tee/tee_fs_key_manager.c
@@ -312,7 +312,7 @@ TEE_Result tee_fs_crypt_block(const TEE_UUID *uuid, uint8_t *out,
 	size_t ctx_size;
 	uint32_t algo = TEE_ALG_AES_CBC_NOPAD;
 
-	DMSG("%scrypt block #%u", (mode == TEE_MODE_ENCRYPT) ? "En" : "De",
+	FMSG("%scrypt block #%u", (mode == TEE_MODE_ENCRYPT) ? "En" : "De",
 	     blk_idx);
 
 	/* Decrypt FEK */

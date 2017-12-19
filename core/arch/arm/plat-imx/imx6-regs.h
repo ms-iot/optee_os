64899cc33a61d8304bb1ac53f9286edc6e930de9
Jordan Rhee <jordanrh@microsoft.com>
2017-12-19 14:20:25 -0800

    iMX6: Implement PSCI shutdown and reboot

--- core/arch/arm/plat-imx/imx6-regs.h
+++ core/arch/arm/plat-imx/imx6-regs.h
@@ -37,11 +37,17 @@
 #define SRC_SCR_CPU_ENABLE_ALL		SHIFT_U32(0x7, 22)
 #define SRC_SCR_CORE1_RST_OFFSET	14
 #define SRC_SCR_CORE1_ENABLE_OFFSET	22
+#define SRC_SCR_WARM_RESET_ENABLE       (1u << 0)
+#define SRC_SCR_MASK_WDOG_RST           0x00000780
+#define SRC_SCR_WDOG_NOTMASKED          (0xA << 7)
+
 #define GIC_BASE			0x00A00000
 #define GICC_OFFSET			0x100
 #define GICD_OFFSET			0x1000
 #define GIC_CPU_BASE			(GIC_BASE + GICC_OFFSET)
 #define GIC_DIST_BASE			(GIC_BASE + GICD_OFFSET)
+#define SNVS_BASE                       0x020CC000
+#define WDOG_BASE                       0x020BC000
 
 #define UART1_BASE			0x02020000
 #define UART2_BASE			0x021E8000
@@ -136,5 +142,60 @@
 #define IMX_CCM_CCGR1_ECSPI2_CLK_SHIFT      2
 #define IMX_CCM_CCGR1_ECSPI2_CLK_ENABLED    \
     (IMX_CGR_CLK_ENABLED << IMX_CCM_CCGR1_ECSPI2_CLK_SHIFT) /* ECSPI2 clock enabled */
+
+/* SNVS */
+#define SNVS_HPLR               0x00000000
+#define SNVS_HPCOMR             0x00000004
+#define SNVS_HPCR               0x00000008
+#define SNVS_HPSICR             0x0000000C
+#define SNVS_HPSVCR             0x00000010
+#define SNVS_HPSR               0x00000014
+#define SNVS_HPSVSR             0x00000018
+#define SNVS_HPHACIVR           0x0000001C
+#define SNVS_HPHACR             0x00000020
+#define SNVS_HPRTCMR            0x00000024
+#define SNVS_HPRTCLR            0x00000028
+#define SNVS_HPTAMR             0x0000002C
+#define SNVS_HPTALR             0x00000030
+#define SNVS_LPLR               0x00000034
+#define SNVS_LPCR               0x00000038
+#define SNVS_LPMKCR             0x0000003C
+#define SNVS_LPSVCR             0x00000040
+#define SNVS_LPTGFCR            0x00000044
+#define SNVS_LPTDCR             0x00000048
+#define SNVS_LPSR               0x0000004C
+#define SNVS_LPSRTCMR           0x00000050
+#define SNVS_LPSRTCLR           0x00000054
+#define SNVS_LPTAR              0x00000058
+#define SNVS_LPSMCMR            0x0000005C
+#define SNVS_LPSMCLR            0x00000060
+#define SNVS_LPPGDR             0x00000064
+#define SNVS_LPGPR              0x00000068
+#define SNVS_LPZMK              0x0000006C
+#define SNVS_HPVIDR1            0x00000BF8
+#define SNVS_HPVIDR2            0x00000BFC
+
+#define SNVS_LPPGDR_INIT        0x41736166
+
+#define SNVS_LPCR_DP_EN         (1u << 5)
+#define SNVS_LPCR_TOP           (1u << 6)
+
+#define SNVS_LPSR_PGD           (1u << 3)
+
+/* Watchdog */
+
+#define WDOG1_WCR               0x00000000
+#define WDOG1_WSR               0x00000002
+#define WDOG1_WRSR              0x00000004
+#define WDOG1_WICR              0x00000006
+#define WDOG1_WMCR              0x00000008
+
+#define WDOG_WCR_WDE            (1u << 2)
+#define WDOG_WCR_WDT            (1u << 3)
+#define WDOG_WCR_SRS            (1u << 4)
+#define WDOG_WCR_WDA            (1u << 5)
+
+#define WDOG_WSR_FEED1          0x5555
+#define WDOG_WSR_FEED2          0xAAAA
 
 #endif // #if defined(CFG_MX6Q)

30e9d9ed33b4bcfb9358dc47ba632a981a3ea012
Jordan Rhee <jordanrh@microsoft.com>
2018-09-25 14:24:45 -0700

    IMX: don't write to disabled UARTs

--- core/drivers/imx_uart.c
+++ core/drivers/imx_uart.c
@@ -92,6 +92,9 @@ static void imx_uart_flush(struct serial_chip *chip)
 {
 	vaddr_t base = chip_to_base(chip);
 
+	if (!(read32(base + UCR1) & UCR1_UARTEN))
+		return;
+
 	while (!(read32(base + UTS) & UTS_TXEMPTY))
 		;
 }
@@ -109,6 +112,9 @@ static int imx_uart_getchar(struct serial_chip *chip)
 static void imx_uart_putc(struct serial_chip *chip, int ch)
 {
 	vaddr_t base = chip_to_base(chip);
+
+	if (!(read32(base + UCR1) & UCR1_UARTEN))
+		return;
 
 	write32(ch, base + UTXD);
 

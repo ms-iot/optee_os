caf40622be769e2dfc75f463441665c1aefc6258
Hernan Gatta <hegatta@microsoft.com>
2019-04-13 00:09:51 -0700

    Enlarge shared memory and TA RAM areas for QEMU ARMv8.

--- core/arch/arm/plat-vexpress/conf.mk
+++ core/arch/arm/plat-vexpress/conf.mk
@@ -101,11 +101,11 @@ ifeq ($(PLATFORM_FLAVOR),qemu_armv8a)
 CFG_TEE_CORE_NB_CORE = 4
 # [0e00.0000 0e0f.ffff] is reserved to early boot
 CFG_TZDRAM_START ?= 0x0e100000
-CFG_TZDRAM_SIZE  ?= 0x00f00000
+CFG_TZDRAM_SIZE  ?= 0x01e00000
 # SHM chosen arbitrary, in a way that it does not interfere
 # with initial location of linux kernel, dtb and initrd.
 CFG_SHMEM_START ?= 0x42000000
-CFG_SHMEM_SIZE  ?= 0x00200000
+CFG_SHMEM_SIZE  ?= 0x0c00000
 # When Secure Data Path is enable, last MByte of TZDRAM is SDP test memory.
 CFG_TEE_SDP_MEM_SIZE ?= 0x00400000
 $(call force,CFG_DT,y)

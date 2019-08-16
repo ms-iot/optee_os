1a93075ec544dba7489a7fbb674b3f4ebeb0af9e
Hernan Gatta <hegatta@microsoft.com>
2019-08-16 00:07:39 -0700

    Modify ELF loader and TA headers to provide RVA at runtime.

--- ta/arch/arm/user_ta_header.c
+++ ta/arch/arm/user_ta_header.c
@@ -88,6 +88,15 @@ const struct ta_head ta_head __section(".ta_head") = {
 	.depr_entry = UINT64_MAX,
 };
 
+volatile struct ta_info ta_info __section(".ta_info") = {
+	.rva = 0,
+};
+
+uintptr_t tainfo_get_rva(void)
+{
+	return ta_info.rva;
+}
+
 /* Keeping the heap in bss */
 uint8_t ta_heap[TA_DATA_SIZE];
 const size_t ta_heap_size = sizeof(ta_heap);
